b4443718f108d1a702814043fb3a7a3e0ee1c3fb
joehoughton <j.houghton8568@student.leedsbeckett.ac.uk>
2016-01-28 19:58:22 +0000

    Display spinner on each ajax request.
    Set one second timeout before it is hidden.

--- production-project.Web/Scripts/app/config/ajax.request.js
+++ production-project.Web/Scripts/app/config/ajax.request.js
@@ -7,21 +7,32 @@
 
   var alertChannel = Radio.channel('alert');
 
+  // show spinner on each ajax call
+  $(document).ajaxStart(function () {
+    $('#spinner').show();
+  });
+
+  function closeSpinner() {
+    $('#spinner').fadeOut();
+  }
+
   // called on each http request and sets the error / success messages
   $(document).ajaxComplete(function (event, xhr) {
     var statusCode = xhr.status;
 
     if (statusCode === 401 || statusCode === 403) {
       Backbone.history.navigate('login', {trigger: true});
-      alertChannel.trigger('error', 'Unauthorised Access');
+      alertChannel.trigger('error', 'Unauthorized Access');
     }
 
     if (statusCode === 400) {
-      alertChannel.trigger('error', 'Unauthorised Access');
+      alertChannel.trigger('error', 'Unauthorized Access');
     }
 
     if (statusCode >= 500) {
       alertChannel.trigger('error', 'The server is not responding');
     }
+    // hide spinner
+    setTimeout(closeSpinner, 100);
   });
 });
